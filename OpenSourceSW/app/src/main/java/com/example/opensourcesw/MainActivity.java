7c34d0ee1d394346f4ce8e0c8e2153495d51eb4f
MinhyeokOh <64846687+MinhyeokOh@users.noreply.github.com>
2023-05-26 21:14:01 +0900

    Add files via upload

--- OpenSourceSW/app/src/main/java/com/example/opensourcesw/MainActivity.java
+++ OpenSourceSW/app/src/main/java/com/example/opensourcesw/MainActivity.java
@@ -41,10 +41,11 @@ import android.content.pm.PackageManager;
 import androidx.core.app.ActivityCompat;
 import android.util.Log;
 import com.google.android.gms.maps.GoogleMap;
-
+import java.io.FileInputStream;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import android.view.ViewGroup;
+import android.location.Location;
 
 public class MainActivity extends AppCompatActivity {
     private final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1;
@@ -67,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
 
     static ArrayList<String> pathList = new ArrayList<>();
     static ArrayList<Bitmap> imgList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -102,8 +104,8 @@ public class MainActivity extends AppCompatActivity {
 
         mapButton.setOnClickListener(new View.OnClickListener() {
 
-                @Override
-                public void onClick(View v) { onMapButtonClicked(); }
+            @Override
+            public void onClick(View v) { onMapButtonClicked(); }
         });
 
         diaryButton.setOnClickListener(new View.OnClickListener() {
@@ -155,16 +157,13 @@ public class MainActivity extends AppCompatActivity {
         ContentValues contentValues = new ContentValues();
         String filePath = "";
 
-        String tags = "";
-        int n = 0;
+        double latitude = 0.0;
+        double longitude = 0.0;
         String[] projection = {
                 MediaStore.Images.Media._ID,
                 MediaStore.Images.Media.RELATIVE_PATH,
                 MediaStore.Images.Media.DISPLAY_NAME,
-                MediaStore.Images.Media.DATE_TAKEN,
-                MediaStore.Images.Media.LATITUDE,
-                MediaStore.Images.Media.LONGITUDE,
-                MediaStore.Images.Media.DESCRIPTION
+                MediaStore.Images.Media.DATE_TAKEN
         };
         String sortOrder = MediaStore.Images.Media._ID + " DESC";
 
@@ -203,20 +202,24 @@ public class MainActivity extends AppCompatActivity {
                 int columnindex = cursor.getColumnIndex(MediaStore.Images.Media.DATE_TAKEN);
                 String dateString = cursor.getString(columnindex);
 
-                tags = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DESCRIPTION));
-                if(tags == ""){
-                    tags = "";
-                }
-
-                double latitude = cursor.getDouble(cursor.getColumnIndex(MediaStore.Images.Media.LATITUDE));
-                double longitude = cursor.getDouble(cursor.getColumnIndex(MediaStore.Images.Media.LONGITUDE));
 
 
                 try{
                     filePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + filePath + display_name;
+
+                    try {
+                        ExifInterface exifInterface = new ExifInterface(filePath);
+                        float[] latLong = new float[2];
+                        if (exifInterface.getLatLong(latLong)) {
+                            latitude = latLong[0];
+                            longitude = latLong[1];
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+
                     File file = new File(filePath);
-
-
 
                     if (file.exists()) {
                         Bitmap bitmap = BitmapFactory.decodeFile(filePath);
@@ -229,12 +232,14 @@ public class MainActivity extends AppCompatActivity {
                         contentValues.put("_id", id);
                         contentValues.put("img", byteArray);
                         contentValues.put("date_taken", dateString);
-                        contentValues.put("tag", tags);
+                        contentValues.put("tag", "");
                         contentValues.put("latitude", latitude);
                         contentValues.put("longitude", longitude);
                         contentValues.put("filePath", filePath);
-                        contentValues.put("diary", " ");
+                        contentValues.put("diary", "");
 
+                        latitude = 0.0;
+                        longitude = 0.0;
 
                         database.insertWithOnConflict("image", null, contentValues, SQLiteDatabase.CONFLICT_IGNORE);
 
@@ -245,13 +250,13 @@ public class MainActivity extends AppCompatActivity {
                 } catch(Exception e){
                     return;
                 }
-                numImage = imgList.size();
-            }
 
-            if(imgList.size() != 0) {
-                showImageList(0);
             }
-            cursor.close();
+        numImage = imgList.size();
+        if(imgList.size() != 0) {
+            showImageList(0);
+        }
+        cursor.close();
 
 
 
@@ -306,10 +311,10 @@ public class MainActivity extends AppCompatActivity {
             }
 
 
-        currentIndex = 0;
-        numImage = imgList.size();
+            currentIndex = 0;
+            numImage = imgList.size();
 
-        showImageList(currentIndex);
+            showImageList(currentIndex);
         }catch(Exception e){
             Toast toastView = Toast.makeText(this, "해당 태그를 가진 이미지가 존재하지 않습니다.", Toast.LENGTH_SHORT);
             toastView.show();
@@ -345,12 +350,12 @@ public class MainActivity extends AppCompatActivity {
                 tv.show();
                 return;
             }
-                currentIndex = 0;
+            currentIndex = 0;
 
 
-                imgList.clear();
+            imgList.clear();
 
-                int index = 0;
+            int index = 0;
             if (cursor.moveToFirst()) {
 
                 do {
@@ -396,120 +401,120 @@ public class MainActivity extends AppCompatActivity {
         }
 
         transaction.commit();
-            fragmentManager.addOnBackStackChangedListener(
-                    new FragmentManager.OnBackStackChangedListener() {
+        fragmentManager.addOnBackStackChangedListener(
+                new FragmentManager.OnBackStackChangedListener() {
 
-                        @Override
-                        public void onBackStackChanged() {
+                    @Override
+                    public void onBackStackChanged() {
 
-                            imgList.clear();
-                            if (fragmentManager.getBackStackEntryCount() == 0) {
+                        imgList.clear();
+                        if (fragmentManager.getBackStackEntryCount() == 0) {
 
-                                String selectedAdd = fragment.getAddressName();
-                                System.out.println(selectedAdd + " 를 선택함");
+                            String selectedAdd = fragment.getAddressName();
+                            System.out.println(selectedAdd + " 를 선택함");
 
-                                nextButton.setVisibility(View.VISIBLE);
-                                mapButton.setVisibility(View.VISIBLE);
-                                diaryButton.setVisibility(View.VISIBLE);
-                                prevButton.setVisibility(View.VISIBLE);
+                            nextButton.setVisibility(View.VISIBLE);
+                            mapButton.setVisibility(View.VISIBLE);
+                            diaryButton.setVisibility(View.VISIBLE);
+                            prevButton.setVisibility(View.VISIBLE);
 
-                                String sql = "SELECT filePath, latitude, longitude FROM image";
-                                Cursor cursor = null;
-                                cursor = database.rawQuery(sql, null);
+                            String sql = "SELECT filePath, latitude, longitude FROM image";
+                            Cursor cursor = null;
+                            cursor = database.rawQuery(sql, null);
 
-                                String sarr1[];
-                                try {
-                                    sarr1 = selectedAdd.split(" ");
-                                } catch (Exception e) {
-                                    return;
-                                }
-                                if (cursor.moveToFirst()) {
-                                    int num = 0;
-                                    do {
-
-                                        int latIndex = cursor.getColumnIndex("latitude");
-                                        double lat = cursor.getDouble(latIndex);
-
-                                        int longIndex = cursor.getColumnIndex("longitude");
-                                        double lng = cursor.getDouble(longIndex);
-
-
-                                        LatLng latLng = new LatLng(lat, lng);
-                                        List<Address> addlist;
-                                        try {
-                                            addlist = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
-                                        } catch (Exception e) {
-                                            e.printStackTrace();
-                                            return;
-                                        }
-                                        if (addlist.size() > 0) {
-                                            Address add = addlist.get(0);
-                                            StringBuilder fullAdd = new StringBuilder();
-                                            for (int i = 0; i <= add.getMaxAddressLineIndex(); i++) {
-                                                fullAdd.append(add.getAddressLine(i));
-                                                if (i < add.getMaxAddressLineIndex()) {
-                                                    fullAdd.append(", ");
-                                                }
-                                            }
-                                            String addName = fullAdd.toString();
-
-                                            String sarr2[];
-                                            try {
-                                                System.out.println(addName);
-                                                sarr2 = addName.split(" ");
-                                            }catch(NullPointerException e){
-                                                continue;
-                                            }
-
-                                            boolean same = true;
-                                            if (sarr1.length < 3 || sarr2.length < 3) {
-                                                int shorter = Math.min(sarr1.length, sarr2.length);
-
-                                                for (int i = 0; i < shorter; i++) {
-                                                    if (!(sarr1[i].equals(sarr2[i]))) {
-                                                        same = false;
-                                                    }
-                                                }
-                                            } else {
-                                                for (int i = 0; i < 3; i++) {
-                                                    if (!(sarr1[i].equals(sarr2[i]))) {
-                                                        same = false;
-                                                    }
-                                                }
-                                            }
-                                            if (same == true) {
-                                                int columnIndex = cursor.getColumnIndex("filepath");
-                                                Bitmap bitmap = BitmapFactory.decodeFile(cursor.getString(columnIndex));
-                                                imgList.add(bitmap);
-                                            }
-                                        }
-
-
-                                    } while (cursor.moveToNext());
-                                    System.out.println("num: " + num);
-                                }
-                                try{
-                                    if (imgList.size() == 0) {
-                                        throw new Exception();
-                                    }
-                                } catch (Exception e) {
-                                    for (int i = 0; i < backUpList.size(); i++) {
-                                        imgList.add(backUpList.get(i));
-                                    }
-                                    showToast();
-                                }finally {
-                                    currentIndex = 0;
-                                    numImage = imgList.size();
-                                    showImageList(currentIndex);
-                                }
+                            String sarr1[];
+                            try {
+                                sarr1 = selectedAdd.split(" ");
+                            } catch (Exception e) {
+                                return;
                             }
+                            if (cursor.moveToFirst()) {
+                                int num = 0;
+                                do {
 
+                                    int latIndex = cursor.getColumnIndex("latitude");
+                                    double lat = cursor.getDouble(latIndex);
+
+                                    int longIndex = cursor.getColumnIndex("longitude");
+                                    double lng = cursor.getDouble(longIndex);
+
+
+                                    LatLng latLng = new LatLng(lat, lng);
+                                    List<Address> addlist;
+                                    try {
+                                        addlist = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                        return;
+                                    }
+                                    if (addlist.size() > 0) {
+                                        Address add = addlist.get(0);
+                                        StringBuilder fullAdd = new StringBuilder();
+                                        for (int i = 0; i <= add.getMaxAddressLineIndex(); i++) {
+                                            fullAdd.append(add.getAddressLine(i));
+                                            if (i < add.getMaxAddressLineIndex()) {
+                                                fullAdd.append(", ");
+                                            }
+                                        }
+                                        String addName = fullAdd.toString();
+
+                                        String sarr2[];
+                                        try {
+                                            System.out.println(addName);
+                                            sarr2 = addName.split(" ");
+                                        }catch(NullPointerException e){
+                                            continue;
+                                        }
+
+                                        boolean same = true;
+                                        if (sarr1.length < 3 || sarr2.length < 3) {
+                                            int shorter = Math.min(sarr1.length, sarr2.length);
+
+                                            for (int i = 0; i < shorter; i++) {
+                                                if (!(sarr1[i].equals(sarr2[i]))) {
+                                                    same = false;
+                                                }
+                                            }
+                                        } else {
+                                            for (int i = 0; i < 3; i++) {
+                                                if (!(sarr1[i].equals(sarr2[i]))) {
+                                                    same = false;
+                                                }
+                                            }
+                                        }
+                                        if (same == true) {
+                                            int columnIndex = cursor.getColumnIndex("filepath");
+                                            Bitmap bitmap = BitmapFactory.decodeFile(cursor.getString(columnIndex));
+                                            imgList.add(bitmap);
+                                        }
+                                    }
+
+
+                                } while (cursor.moveToNext());
+                                System.out.println("num: " + num);
+                            }
+                            try{
+                                if (imgList.size() == 0) {
+                                    throw new Exception();
+                                }
+                            } catch (Exception e) {
+                                for (int i = 0; i < backUpList.size(); i++) {
+                                    imgList.add(backUpList.get(i));
+                                }
+                                showToast();
+                            }finally {
+                                currentIndex = 0;
+                                numImage = imgList.size();
+                                showImageList(currentIndex);
+                            }
                         }
 
-                    });
+                    }
+
+                });
 
 
-        }
+    }
 
 
 
